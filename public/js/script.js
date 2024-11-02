58cdf9e24cb904981ce76bba788f9610df950662
Vijay <vijaymudaliyar224@gmail.com>
2024-11-01 23:12:14 -0700

    hamburger updation for devices

--- public/js/script.js
+++ public/js/script.js
@@ -39,8 +39,8 @@ function ham_close() {
 
     crossIcon.style.display = "none";
     hamIcon.style.display = "block";
-    hamStyle.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
-    hamStyle.style.zIndex = "500";
+    // hamStyle.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    hamStyle.style.zIndex = "2000";
     bodyScroll.style.overflowY= "scroll";
 
 }
